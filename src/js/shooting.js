6805dbab6bb6466d0d42e9a2dc6f54aea1654760
gangelion <gangelion8@gmail.com>
2020-06-09 14:33:45 +0900

    enemyを10匹生成

--- src/js/shooting.js
+++ src/js/shooting.js
@@ -4,6 +4,13 @@ let canvas;
 let ctx;
 let img_player;
 let img_enemy;
+let player_x;
+let player_y;
+
+
+window.onkeydown = function(e) {
+  console.log(e);
+};
 
 window.onload = function() {
   canvas = document.getElementById('screen');
@@ -12,10 +19,15 @@ window.onload = function() {
   img_player = document.getElementById('player');
   img_enemy = document.getElementById('enemy');
 
-  ctx.drawImage(img_player, 20, 50);
-  ctx.drawImage(img_enemy,
-                Math.random() * (canvas.width - img_enemy.width),
-                Math.random() * (canvas.height - img_enemy.height));
+  player_x = (canvas.width - player.width) / 2;
+  player_y = (canvas.height - player.height) - 20;
+
+  ctx.drawImage(img_player, player_x, player_y);
+  for (let i = 0; i < 10; i++) {
+    ctx.drawImage(img_enemy,
+                  Math.random() * (canvas.width - img_enemy.width),
+                  Math.random() * (canvas.height - img_enemy.height));
+  }
 
 
 };
