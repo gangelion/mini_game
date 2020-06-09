66d8c98a17a9b897d7ce5c58dcc183e79bee9034
gangelion <gangelion8@gmail.com>
2020-06-09 14:07:30 +0900

    canvasの追加

--- src/js/shooting.js
+++ src/js/shooting.js
@@ -1,1 +1,18 @@
 "use strict"
+
+let canvas;
+let ctx;
+
+window.onload = function() {
+  canvas = this.document.getElementById('screen');
+
+  ctx = canvas.getContext('2d');
+
+  ctx.fillStyle = '#fff';
+
+  ctx.fillRect(8, 5, 20, 30);
+
+  ctx.strokeStyle = '#f00';
+
+  ctx.strokeRect(40, 55, 90, 10);
+};
